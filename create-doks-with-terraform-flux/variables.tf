5bb306ac12ccc6ede10304789f7b8a5427e78fd3
Cristian Marius Tiutiu <v-ctiutiu@digitalocean.com>
2021-12-10 18:47:30 +0200

    removed redundant 'github.com' string from github_ssh_pub_key variable

--- create-doks-with-terraform-flux/variables.tf
+++ create-doks-with-terraform-flux/variables.tf
@@ -47,7 +47,7 @@ variable "github_token" {
 variable "github_ssh_pub_key" {
   description = "GitHub SSH public key"
   type        = string
-  default     = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
+  default     = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
 }
 
 variable "git_repository_name" {
